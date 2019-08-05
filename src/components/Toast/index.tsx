cc2d0c19f0af68aa6220732b65c439b2d445a472
AMI TANOUE <ami@AMInoMacBook-Pro.local>
2019-08-05 17:02:19 +0900

    refactor: Toastのbox-shadowを修正

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -58,7 +58,7 @@ const InfoOuter = styled.div`
     background-color: ${props => props.theme.colors.utilities.highlightGreen};
     padding: 12px;
     border-radius: 6px;
-    box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.16);
+    box-shadow: ${props => props.theme.shadows.L5};
 `
 const WarningOuter = styled.div`
     display: inline-flex;
