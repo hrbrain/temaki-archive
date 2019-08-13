896d94e1971b9477d82b37936a40c2dd8f1fe642
AMI TANOUE <ami@AMInoMacBook-Pro.local>
2019-08-13 12:49:17 +0900

    refactor: 共通している処理を共通化

--- src/components/MeatballKebabMenu/index.tsx
+++ src/components/MeatballKebabMenu/index.tsx
@@ -9,7 +9,8 @@ import * as Icon from '~/components/Icon'
  */
 
 type Props = {
-    type: 'm-top' | 'm-bottom' | 'k-top' | 'k-bottom'
+    iconSrc: string
+    position: 'top' | 'bottom'
     listItems: Item[]
 }
 
@@ -18,125 +19,97 @@ type Item = {
     onClick: (e: React.MouseEvent) => void
 }
 
-export const Component = React.memo<Props>(({ type, listItems }) => {
-    const [isShow, setIsShow] = React.useState<boolean>(false)
-    const handleClick = React.useCallback(
-        (_: React.MouseEvent<HTMLDivElement>) => {
-            setIsShow(!isShow)
-        },
-        [isShow]
-    )
-    const renderListItem = (listItem: Item) => (
-        <ListItem onClick={listItem.onClick} key={listItem.item}>
-            {listItem.item}
-        </ListItem>
-    )
+type MeatballMenuProps = {
+    position: 'top' | 'bottom'
+    listItems: Item[]
+}
 
-    switch (type) {
-        case 'm-top':
-            return (
-                <Outer>
-                    <Meatball className="top" onClick={handleClick}>
-                        {/* TODO: アイコンをミートボールに変える */}
-                        <MeatballItem svg={IconFiles.icons.MenuV} size="24px" />
-                    </Meatball>
+export const MeatballMenu = ({ position, listItems }: MeatballMenuProps) => (
+    <Component
+        position={position}
+        listItems={listItems}
+        // TODO: アイコンをミートボールに変える
+        iconSrc={IconFiles.icons.MenuV}
+    />
+)
 
-                    {isShow && (
-                        <List className="{[!isShow ? 'hidden' : null].join(' ')}, top">
-                            {listItems.map(renderListItem)}
-                        </List>
-                    )}
-                </Outer>
-            )
-        case 'm-bottom':
-            return (
-                <Outer>
-                    {isShow && (
-                        <List className="{[!isShow ? 'hidden' : null].join(' ')}, bottom">
-                            {listItems.map(renderListItem)}
-                        </List>
-                    )}
+type KebabMenuProps = {
+    position: 'top' | 'bottom'
+    listItems: Item[]
+}
 
-                    <Meatball className="bottom" onClick={handleClick}>
-                        {/* TODO: アイコンをミートボールに変える */}
-                        <MeatballItem svg={IconFiles.icons.MenuV} size="24px" />
-                    </Meatball>
-                </Outer>
-            )
-        case 'k-top':
-            return (
-                <Outer>
-                    <Meatball className="top" onClick={handleClick}>
-                        <MeatballItem svg={IconFiles.icons.MenuV} size="24px" />
-                    </Meatball>
+export const KebabMenu = ({ position, listItems }: KebabMenuProps) => (
+    <Component
+        position={position}
+        listItems={listItems}
+        iconSrc={IconFiles.icons.MenuV}
+    />
+)
 
-                    {isShow && (
-                        <List className="{[!isShow ? 'hidden' : null].join(' ')}, top">
-                            {listItems.map(renderListItem)}
-                        </List>
-                    )}
-                </Outer>
-            )
-        case 'k-bottom':
-            return (
-                <Outer>
-                    {isShow && (
-                        <List className="{[!isShow ? 'hidden' : null].join(' ')}, bottom">
-                            {listItems.map(renderListItem)}
-                        </List>
-                    )}
+export const Component = React.memo<Props>(
+    ({ iconSrc, position, listItems }) => {
+        const [isShow, setIsShow] = React.useState<boolean>(false)
+        const handleClick = React.useCallback(
+            (_: React.MouseEvent<HTMLDivElement>) => {
+                setIsShow(!isShow)
+            },
+            [isShow]
+        )
+        const renderListItem = (listItem: Item) => (
+            <ListItem onClick={listItem.onClick} key={listItem.item}>
+                {listItem.item}
+            </ListItem>
+        )
 
-                    <Meatball className="bottom" onClick={handleClick}>
-                        <MeatballItem svg={IconFiles.icons.MenuV} size="24px" />
-                    </Meatball>
-                </Outer>
-            )
+        return (
+            <Outer>
+                <Meatball className={position} onClick={handleClick}>
+                    <MeatballItem svg={iconSrc} size="24px" />
+                </Meatball>
+                <List className={`${[!isShow ? 'hidden' : position]}`}>
+                    {listItems.map(renderListItem)}
+                </List>
+            </Outer>
+        )
     }
-})
+)
 
 /**
  * style
  */
-const Outer = styled.div``
+const Outer = styled.div`
+    height: 300px;
+    position: relative;
+`
 const Meatball = styled.div`
     cursor: pointer;
-    position: relative;
+    position: absolute;
     &.top {
-        position: absolute;
         top: 0;
         right: 0;
     }
     &.bottom {
-        position: absolute;
         bottom: 0;
         right: 0;
     }
 `
 const MeatballItem = styled(Icon.Component)``
 const List = styled.ul`
+    position: absolute;
     max-width: 140px;
     background: ${props => props.theme.colors.grayScale.S0};
     border-radius: 6px;
     box-shadow: ${props => props.theme.shadows.L5};
     padding: 12px;
-    visibility: visible;
-    transform: scale(1);
-    opacity: 1;
-    transition: 0.2s;
     &.top {
-        position: absolute;
         top: 24px;
         right: 0;
     }
     &.bottom {
-        position: absolute;
         bottom: 24px;
         right: 0;
     }
     &.hidden {
-        opacity: 0;
-        transform: scale(0);
-        visibility: hidden;
     }
 `
 const ListItem = styled.li`
