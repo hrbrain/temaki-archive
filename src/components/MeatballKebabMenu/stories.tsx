6994ac4ae5a14cf6577e7ba42031168f5bb098da
AMI TANOUE <ami@AMInoMacBook-Pro.local>
2019-08-13 12:49:17 +0900

    refactor: 共通している処理を共通化

--- src/components/MeatballKebabMenu/stories.tsx
+++ src/components/MeatballKebabMenu/stories.tsx
@@ -4,25 +4,41 @@ import * as MeatballKebabMenu from './index'
 import { text, select } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 
-storiesOf('Components|MeatballKebabMenu', module).add('all', () => {
-    return (
-        <div className="m-10">
-            <MeatballKebabMenu.Component
-                type={select(
-                    'Place',
-                    {
-                        meatball_top: 'm-top',
-                        meatball_bottom: 'm-bottom',
-                        kebab_top: 'k-top',
-                        kebab_bottom: 'k-bottom'
-                    },
-                    'm-top'
-                )}
-                listItems={menuItems}
-            />
-        </div>
-    )
-})
+storiesOf('Components|MeatballKebabMenu', module)
+    .add('MeatballMenu', () => {
+        return (
+            <div className="m-10">
+                <MeatballKebabMenu.MeatballMenu
+                    position={select(
+                        'Position',
+                        {
+                            top: 'top',
+                            bottom: 'bottom'
+                        },
+                        'top'
+                    )}
+                    listItems={menuItems}
+                />
+            </div>
+        )
+    })
+    .add('KebabMenu', () => {
+        return (
+            <div className="m-10">
+                <MeatballKebabMenu.KebabMenu
+                    position={select(
+                        'Position',
+                        {
+                            top: 'top',
+                            bottom: 'bottom'
+                        },
+                        'top'
+                    )}
+                    listItems={menuItems}
+                />
+            </div>
+        )
+    })
 const menuItems = [
     { item: text('text1', 'リスト1'), onClick: action('onClick1') },
     { item: text('text2', 'リスト2'), onClick: action('onClick2') },
