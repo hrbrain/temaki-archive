c916c729698575f766eef5cde42f8805c2cf558b
makino jun <makino.jun@hrbrain.co.jp>
2019-08-28 12:26:11 +0900

    fix: optionKnob()を使って選択を変更できるよう修正

--- src/components/DropdownMultiple/stories.tsx
+++ src/components/DropdownMultiple/stories.tsx
@@ -1,45 +1,37 @@
-import { text, number, boolean } from '@storybook/addon-knobs'
+import { text, number, boolean, optionsKnob } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import * as DropdownSingle from './index'
 
-storiesOf('Components|DropdownMultiple', module)
-    .add('未選択時', () => (
-        <div className="ml-20 mt-10">
-            <DropdownSingle.Component
-                placeholder={text('placeholder', '選択してください')}
-                items={[
-                    { text: 'りんご' },
-                    { text: 'いちご' },
-                    { text: 'バナナ' },
-                    { text: 'メロン' },
-                    { text: 'さくらんぼ' },
-                    { text: 'ぶどう' }
-                ]}
-                selected={[]}
-                isError={boolean('isError', false)}
-                width={number('width', 200)}
-                onClickItem={action('text')}
-            />
-        </div>
-    ))
-    .add('選択時', () => (
-        <div className="ml-20 mt-10">
-            <DropdownSingle.Component
-                placeholder={text('placeholder', '選択してください')}
-                items={[
-                    { text: 'りんご' },
-                    { text: 'いちご' },
-                    { text: 'バナナ' },
-                    { text: 'メロン' },
-                    { text: 'さくらんぼ' },
-                    { text: 'ぶどう' }
-                ]}
-                selected={['りんご', 'バナナ', 'メロン']}
-                isError={boolean('isError', false)}
-                width={number('width', 200)}
-                onClickItem={action('text')}
-            />
-        </div>
-    ))
+storiesOf('Components|DropdownMultiple', module).add('Multi', () => (
+    <div className="ml-20 mt-10">
+        <DropdownSingle.Component
+            placeholder={text('placeholder', '選択してください')}
+            items={[
+                { text: 'りんご' },
+                { text: 'いちご' },
+                { text: 'バナナ' },
+                { text: 'メロン' },
+                { text: 'さくらんぼ' },
+                { text: 'ぶどう' }
+            ]}
+            selected={optionsKnob(
+                'selected',
+                {
+                    りんご: 'りんご',
+                    いちご: 'いちご',
+                    バナナ: 'バナナ',
+                    メロン: 'メロン',
+                    さくらんぼ: 'さくらんぼ',
+                    ぶどう: 'ぶどう'
+                },
+                [],
+                { display: 'check' }
+            )}
+            isError={boolean('isError', false)}
+            width={number('width', 200)}
+            onClickItem={action('text')}
+        />
+    </div>
+))
