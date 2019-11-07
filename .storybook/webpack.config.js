a7f48b6a4cfa22b2d5088f80b42fdc652a54f6c3
Bausmert Robert <bausmertrobert@BausmertnoMacBook-Pro.local>
2019-11-07 17:39:00 +0900

    chore: add css-loader and style-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,40 +1,46 @@
 const path = require('path')
 
 module.exports = ({ config }) => {
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '~': path.resolve(__dirname, '../src/')
-  }
-
-  config.module.rules = [
-    {
-      test: /\.stories\.tsx?$/,
-      use: [
-        {
-          loader: require.resolve('@storybook/addon-storysource/loader'),
-          options: { parser: 'typescript' }
-        }
-      ],
-      enforce: 'pre'
-    },
-    {
-      test: /\.tsx?$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader')
-        }
-      ]
-    },
-    {
-      test: /\.svg$/,
-      loader: 'raw-loader'
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        '~': path.resolve(__dirname, '../src/')
     }
-  ]
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '~': path.resolve(__dirname, '../src')
-  }
 
-  config.resolve.extensions.push('.ts', '.tsx')
-  return config
+    config.module.rules = [
+        {
+            test: /\.stories\.tsx?$/,
+            use: [
+                {
+                    loader: require.resolve(
+                        '@storybook/addon-storysource/loader'
+                    ),
+                    options: { parser: 'typescript' }
+                }
+            ],
+            enforce: 'pre'
+        },
+        {
+            test: /\.tsx?$/,
+            use: [
+                {
+                    loader: require.resolve('ts-loader')
+                }
+            ]
+        },
+        {
+            test: /\.svg$/,
+            loader: 'raw-loader'
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        '~': path.resolve(__dirname, '../src')
+    }
+
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
 }
