0eaba440ec7736249c9ea5820120395f8c152714
Bausmert Robert <bausmertrobert@BausmertnoMacBook-Pro.local>
2019-11-29 19:32:18 +0900

    chore: ignore modal storyshot test

--- src/__test__/storyshots.spec.ts
+++ src/__test__/storyshots.spec.ts
@@ -1,3 +1,9 @@
 import * as Storyshots from '@storybook/addon-storyshots'
 
+<<<<<<< HEAD
 Storyshots.default({ storyKindRegex: /^((?!.*?DateRangePicker).)*$/ })
+=======
+Storyshots.default({
+    storyKindRegex: /^((?!Modal).)*$/
+})
+>>>>>>> chore: ignore modal storyshot test
