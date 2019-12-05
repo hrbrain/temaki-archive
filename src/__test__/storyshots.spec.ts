ad35edcfd5b6db72b4d65ee7169b85e96c612ac4
Bausmert Robert <bausmertrobert@BausmertnoMacBook-Pro.local>
2019-12-05 15:06:04 +0900

    fix: font size

--- src/__test__/storyshots.spec.ts
+++ src/__test__/storyshots.spec.ts
@@ -1,9 +1,3 @@
 import * as Storyshots from '@storybook/addon-storyshots'
 
-<<<<<<< HEAD
-Storyshots.default({ storyKindRegex: /^((?!.*?DateRangePicker).)*$/ })
-=======
-Storyshots.default({
-    storyKindRegex: /^((?!Modal).)*$/
-})
->>>>>>> chore: ignore modal storyshot test
+Storyshots.default({ storyKindRegex: /^((?!.*?DateRangePicker|Modal).)*$/ })
