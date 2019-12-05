b3177f18dcfa2979bbe70058f5e316882bf49703
Bausmert Robert <bausmertrobert@BausmertnoMacBook-Pro.local>
2019-12-05 14:39:13 +0900

    test: wrote tests

---
+++ src/components/Modal/index.spec.tsx
@@ -0,0 +1,52 @@
+import * as Enzyme from 'enzyme'
+import * as React from 'react'
+import { act } from 'react-dom/test-utils'
+import { mountWithTheme } from '~/__test__/utils'
+import * as Modal from './body'
+
+describe('Modal component', () => {
+    let wrapper: Enzyme.ReactWrapper
+    let title = 'ロール作成'
+    let buttons = [
+        {
+            type: 'box' as const,
+            colorType: 'primary' as const,
+            text: '登録'
+        }
+    ]
+    let mockOnClose: jest.Mock
+
+    beforeEach(() => {
+        mockOnClose = jest.fn()
+        act(() => {
+            wrapper = mountWithTheme(
+                <Modal.Component
+                    title={title}
+                    buttons={buttons}
+                    onClose={mockOnClose}
+                >
+                    コンテンツ
+                </Modal.Component>
+            )
+        })
+    })
+
+    it('should be defined', () => {
+        expect(wrapper).toBeDefined()
+    })
+
+    it('should call onClose', () => {
+        act(() => {
+            wrapper.find('div[data-test="closeIcon"]').simulate('click')
+        })
+        expect(mockOnClose).toHaveBeenCalled()
+    })
+
+    it('Should render a ', () => {
+        buttons.forEach((_, index) => {
+            expect(
+                wrapper.find(`button[data-test="button${index}"]`)
+            ).toHaveLength(1)
+        })
+    })
+})
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,86 +1,24 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import styled from '~/modules/theme'
 
-import * as Icon from '~/components/Icon'
 import * as Button from '~/components/Button'
-
-import * as IconFiles from '~/lib/iconFiles'
+import * as Body from './body'
 
 type Props = {
     title: string
     buttons?: (Button.Props & { text: string })[]
+    onClose: () => void
 }
 
 const portalId = document.getElementById('modal') as HTMLDivElement
 
 export const Component: React.FC<Props> = props => {
     return ReactDOM.createPortal(
-        <Background>
-            <Wrap>
-                <Head>
-                    <div>{props.title}</div>
-                    <Icon.Component svg={IconFiles.icons.Close} size="24" />
-                </Head>
-                <Body>{props.children}</Body>
-                {props.buttons && (
-                    <Footer>
-                        {props.buttons.map((buttonProps, index) => (
-                            <StyledButton key={index} {...buttonProps}>
-                                {buttonProps.text}
-                            </StyledButton>
-                        ))}
-                    </Footer>
-                )}
-            </Wrap>
-        </Background>,
+        <Body.Component
+            title={props.title}
+            buttons={props.buttons}
+            onClose={props.onClose}
+        />,
         portalId
     )
 }
-
-const Background = styled.div`
-    position: fixed;
-    z-index: 600;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.6);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
-const Wrap = styled.div`
-    width: 678px;
-    max-height: 678px;
-    overflow: scroll;
-    background-color: ${props => props.theme.colors.grayScale.S0};
-    border-radius: 8px;
-`
-
-const Head = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 24px;
-    border-bottom: 1px solid ${props => props.theme.colors.grayScale.S20};
-`
-
-const Body = styled.div`
-    padding: 24px;
-`
-
-const Footer = styled.div`
-    padding: 24px;
-    border-top: 1px solid ${props => props.theme.colors.grayScale.S20};
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-`
-
-const StyledButton = styled(Button.Component)`
-    & + & {
-        margin-left: 16px;
-    }
-`
