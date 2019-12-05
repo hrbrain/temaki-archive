9221706febfa497aba1d93dd4332203b07e0c07a
Bausmert Robert <bausmertrobert@BausmertnoMacBook-Pro.local>
2019-12-05 14:39:13 +0900

    test: wrote tests

---
+++ src/components/Modal/body.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react'
+import styled from '~/modules/theme'
+
+import * as Icon from '~/components/Icon'
+import * as Button from '~/components/Button'
+
+import * as IconFiles from '~/lib/iconFiles'
+
+type Props = {
+    title: string
+    buttons?: (Button.Props & { text: string })[]
+    onClose: () => void
+}
+
+export const Component: React.FC<Props> = props => {
+    return (
+        <Background>
+            <Wrap>
+                <Head>
+                    <Title>{props.title}</Title>
+                    <IconWrap onClick={props.onClose} data-test="closeIcon">
+                        <Icon.Component svg={IconFiles.icons.Close} size="24" />
+                    </IconWrap>
+                </Head>
+                <Body>{props.children}</Body>
+                {props.buttons && (
+                    <Footer>
+                        {props.buttons.map((buttonProps, index) => (
+                            <StyledButton
+                                key={index}
+                                data-test={`button${index}`}
+                                {...buttonProps}
+                            >
+                                {buttonProps.text}
+                            </StyledButton>
+                        ))}
+                    </Footer>
+                )}
+            </Wrap>
+        </Background>
+    )
+}
+
+const Background = styled.div`
+    position: fixed;
+    z-index: 600;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Wrap = styled.div`
+    width: 678px;
+    max-height: 678px;
+    overflow: scroll;
+    background-color: ${props => props.theme.colors.grayScale.S0};
+    border-radius: 8px;
+`
+
+const Head = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 24px;
+    border-bottom: 1px solid ${props => props.theme.colors.grayScale.S20};
+`
+
+const Title = styled.div`
+    font-weight: bold;
+`
+
+const IconWrap = styled.div`
+    cursor: pointer;
+`
+
+const Body = styled.div`
+    padding: 24px;
+`
+
+const Footer = styled.div`
+    padding: 24px;
+    border-top: 1px solid ${props => props.theme.colors.grayScale.S20};
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const StyledButton = styled(Button.Component)`
+    & + & {
+        margin-left: 16px;
+    }
+`
--- src/components/Modal/stories.tsx
+++ src/components/Modal/stories.tsx
@@ -1,5 +1,5 @@
-// import { action } from '@storybook/addon-actions'
-// import { boolean, text } from '@storybook/addon-knobs'
+import * as Actions from '@storybook/addon-actions'
+import * as Knobs from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import * as Modal from './index'
@@ -8,21 +8,17 @@ storiesOf('Components|Modal', module).add('all', () => {
     return (
         <div className="m-10">
             <Modal.Component
-                title="ロール作成"
+                title={Knobs.text('title', 'ロール作成')}
                 buttons={[
                     {
                         type: 'box',
                         colorType: 'primary',
-                        text: 'asdsad'
-                    },
-                    {
-                        type: 'box',
-                        colorType: 'primary',
-                        text: 'asdsad222'
+                        text: '登録'
                     }
                 ]}
+                onClose={Actions.action('onClose')}
             >
-                adsasd
+                コンテンツ
             </Modal.Component>
         </div>
     )
