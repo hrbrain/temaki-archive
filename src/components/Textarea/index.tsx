79bb8c845f2151629aaead99fe7bedc224dedf95
hrb-ando-kaoru <ando.kaoru@hrbrain.co.jp>
2019-12-06 21:02:04 +0900

    DesignFix: Fixed border color and placeholder color

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -41,7 +41,7 @@ export { Component }
  */
 
 const Textarea = styled(TextareaAutosize)<{ errored?: boolean }>`
-    border: 1px solid ${props => props.theme.colors.primary.default};
+    border: 1px solid ${props => props.theme.colors.grayScale.S10};
     border-radius: 6px;
     width: 280px;
     padding: 8px;
@@ -49,6 +49,10 @@ const Textarea = styled(TextareaAutosize)<{ errored?: boolean }>`
     font-size: 14px;
     resize: none;
     outline: none;
+    color: ${props => props.theme.colors.grayScale.S100};
+    &::placeholder {
+        color: ${props => props.theme.colors.grayScale.S20};
+    }
 
     &:focus {
         border-color: ${props =>
