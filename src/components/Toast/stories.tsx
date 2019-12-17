d7902ac31f7bc6db6aa44831868a7d7a2694dd3e
hrb-bacigalupo-diego <diego.bacigalupo@hrbrain.co.jp>
2019-12-17 16:07:33 +0900

    feature:add close button to warning type toast

--- src/components/Toast/stories.tsx
+++ src/components/Toast/stories.tsx
@@ -4,23 +4,44 @@ import * as React from 'react'
 import * as Toast from './index'
 import * as Actions from '@storybook/addon-actions'
 
-storiesOf(`Components|Toast`, module).add('all', () => (
-    <div className="m-10">
-        <Toast.Component
-            label={text('Label', 'ラベル')}
-            text={text(
-                'Text',
-                'メールアドレスまたはパスワードに誤りがあります。もう一度入力してください。'
-            )}
-            type={select(
-                'Type',
-                {
-                    info: 'info',
-                    warning: 'warning'
-                },
-                'info'
-            )}
-            onClickClose={Actions.action('onClickClose')}
-        />
-    </div>
-))
+storiesOf(`Components|Toast`, module)
+    .add('info', () => (
+        <div className="m-10">
+            <Toast.Component
+                label={text('Label', 'ラベル')}
+                text={text(
+                    'Text',
+                    'メールアドレスまたはパスワードに誤りがあります。もう一度入力してください。'
+                )}
+                type={select(
+                    'Type',
+                    {
+                        info: 'info',
+                        warning: 'warning'
+                    },
+                    'info'
+                )}
+                onClickClose={Actions.action('onClickClose')}
+            />
+        </div>
+    ))
+    .add('warning', () => (
+        <div className="m-10">
+            <Toast.Component
+                label={text('Label', 'ラベル')}
+                text={text(
+                    'Text',
+                    'メールアドレスまたはパスワードに誤りがあります。もう一度入力してください。'
+                )}
+                type={select(
+                    'Type',
+                    {
+                        info: 'info',
+                        warning: 'warning'
+                    },
+                    'warning'
+                )}
+                onClickClose={Actions.action('onClickClose')}
+            />
+        </div>
+    ))
