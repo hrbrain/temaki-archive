aee0dfbc18f0a57c44b7b8a004f5aee3e77dc290
Souma Suzuki <subayai.kobashiri@gmail.com>
2020-03-31 20:54:34 +0900

    chore: Change config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import resolve from 'rollup-plugin-node-resolve'
 import cjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
+import alias from '@rollup/plugin-alias'
+import svg from 'rollup-plugin-svg'
+import builtIns from 'rollup-plugin-node-builtins'
+import path from 'path'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
@@ -24,6 +28,13 @@ const config = {
     ],
     plugins: [
         resolve({ extensions }),
+        builtIns(),
+        svg(),
+        alias({
+            entries: [
+                { find: '~', replacement: path.resolve(__dirname, 'src') }
+            ]
+        }),
         cjs({
             include: 'node_modules/**',
             namedExports: {
@@ -35,17 +46,28 @@ const config = {
                     'forwardRef',
                     'useImperativeHandle',
                     'Fragment',
+                    'useState',
                     'useRef',
                     'useReducer',
                     'useCallback',
                     'useEffect',
-                    'useMemo'
+                    'useLayoutEffect',
+                    'useMemo',
+                    'memo'
                 ],
-                'node_modules/react-dom/index.js': ['render', 'hydrate'],
+                'node_modules/react-dom/index.js': [
+                    'render',
+                    'hydrate',
+                    'createPortal'
+                ],
                 'node_modules/react-is/index.js': [
                     'isElement',
                     'isValidElementType',
                     'ForwardRef'
+                ],
+                'node_modules/react-dates/index.js': [
+                    'SingleDatePicker',
+                    'DateRangePicker'
                 ]
             }
         }),
