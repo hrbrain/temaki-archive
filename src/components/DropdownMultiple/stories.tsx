c2aeaf004384808c8bf3ac9ae1814ffcf4e004cc
hrb-bacigalupo-diego <diego.bacigalupo@hrbrain.co.jp>
2020-05-27 11:56:37 +0900

    test: fix stories

--- src/components/DropdownMultiple/stories.tsx
+++ src/components/DropdownMultiple/stories.tsx
@@ -1,22 +1,31 @@
-import { text, boolean } from '@storybook/addon-knobs'
+import { text, boolean, optionsKnob } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import * as DropdownSingle from './index'
 
+const items = [
+    { value: '1', text: 'りんご' },
+    { value: '2', text: 'いちご' },
+    { value: '3', text: 'バナナ' },
+    { value: '4', text: 'メロン' },
+    { value: '5', text: 'さくらんぼ' },
+    { value: '6', text: 'ぶどう' }
+]
+
+const itemOptions = items.reduce(
+    (accum, item) => ({ ...accum, [item.text]: item.value }),
+    {}
+)
+
 storiesOf('Components|DropdownMultiple', module).add('Multi', () => (
     <div className="ml-20 mt-10">
         <DropdownSingle.Component
             placeholder={text('placeholder', '選択してください')}
-            items={[
-                { value: '1', text: 'りんご' },
-                { value: '2', text: 'いちご' },
-                { value: '3', text: 'バナナ' },
-                { value: '4', text: 'メロン' },
-                { value: '5', text: 'さくらんぼ' },
-                { value: '6', text: 'ぶどう' }
-            ]}
-            values={[]}
+            items={items}
+            values={optionsKnob('selected', itemOptions, [], {
+                display: 'check'
+            })}
             isError={boolean('isError', false)}
             errorMessage={text('ErrorMessage', '')}
             width={text('width', '250px')}
