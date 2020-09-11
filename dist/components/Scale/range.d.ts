0b8acb62ab3f8abb882f170c0f126509a2d93d5f
NatsuboriYuumi <natsubori.yuumi@hrbrain.co.jp>
2020-09-11 16:42:53 +0900

    delete Scale folder

--- dist/components/Scale/range.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-declare type DisplayTypeProps = 'Horizontal' | 'Vertical';
-declare type Props = {
-    scale: number;
-    displayType: DisplayTypeProps;
-    children: React.ReactNode;
-};
-export declare const Component: React.NamedExoticComponent<Props>;
-export {};
