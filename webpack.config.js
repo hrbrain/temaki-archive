11493021bd0d8e8b087bb82bc1cd17bc958ca538
hrb-natsubori-yuumi <62980599+hrb-natsubori-yuumi@users.noreply.github.com>
2020-10-01 20:14:50 +0900

    Update webpack.config.js
    Co-authored-by: hrb-sakuraba-hiroki <62973955+hrb-sakuraba-hiroki@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
                 ]
             },
             {
-                test: /.*\.(gif|jpe?g|png)$/,
+                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
                 use: [
                     {
                         loader: 'file-loader',
