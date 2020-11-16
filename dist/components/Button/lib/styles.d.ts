cf9f6de06233c1cfc4c1a2a342201d564f92ec96
kenjimorita <morita.kenji@hrbrain.co.jp>
2020-11-16 10:39:59 +0900

    fix: secondary

--- dist/components/Button/lib/styles.d.ts
+++ dist/components/Button/lib/styles.d.ts
@@ -7,6 +7,7 @@ declare type CreateCSSFromColorType = {
     border?: string | null;
 };
 export declare const createCSSFromColorType: ({ base, hovered, activated, text, border }: CreateCSSFromColorType) => string;
+export declare const createCSSFromColorTypeSecondary: ({ base, hovered, text, border }: CreateCSSFromColorType) => string;
 export declare const buttonBaseMixin = "\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    border-style: none;\n    padding: 0;\n    cursor: pointer;\n    box-shadow: none;\n    outline: none;\n";
 export declare const rippleEffectMixin: ({ theme }: {
     theme: RequiredThemeProps;
