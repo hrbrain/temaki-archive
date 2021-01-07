0495519da6771e47d995c216f6a94a796d9ce697
hrb-yamamoto-katsuya <yamamoto.katsuya@hrbrain.co.jp>
2021-01-07 18:55:03 +0900

    chore: storyを追加

--- src/components/Illustration/stories.tsx
+++ src/components/Illustration/stories.tsx
@@ -5,12 +5,30 @@ import * as React from 'react'
 import * as IllustrationFiles from '~/lib/illustrationFiles'
 import * as Illustration from './index'
 
-Storybook.storiesOf('components|Illustration', module).add('Standard', () => (
-    <Illustration.Component
-        svg={IllustrationFiles.illustrations.Sample}
-        size={{
-            width: text('Size Width', '401px'),
-            height: text('Size Height', '276px')
+const SVGComponent = () => (
+    <div
+        dangerouslySetInnerHTML={{
+            __html: IllustrationFiles.illustrations.Sample
         }}
-    />
-))
+    ></div>
+)
+
+Storybook.storiesOf('components|Illustration', module)
+    .add('SVG as string', () => (
+        <Illustration.Component
+            svg={IllustrationFiles.illustrations.Sample}
+            size={{
+                width: text('Size Width', '401px'),
+                height: text('Size Height', '276px')
+            }}
+        />
+    ))
+    .add('SVG as Element', () => (
+        <Illustration.Component
+            svg={<SVGComponent />}
+            size={{
+                width: text('Size Width', '401px'),
+                height: text('Size Height', '276px')
+            }}
+        />
+    ))
