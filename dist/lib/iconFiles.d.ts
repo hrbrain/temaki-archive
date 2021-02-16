9bf3f82ab36000200137a4cbcfcd5a09e6b5a9fa
hrb-yamamoto-katsuya <67670052+hrb-yamamoto-katsuya@users.noreply.github.com>
2021-02-16 19:48:50 +0900

    feat: check-filled icon追加 (#215)
    * feat: check-filled icon追加
    
    * test: update snapshot
    
    * feat: id fillを追加

--- dist/lib/iconFiles.d.ts
+++ dist/lib/iconFiles.d.ts
@@ -6,6 +6,7 @@ export declare const icons: {
     Avatar: string;
     BlackBoard: string;
     Calendar: string;
+    CheckFilled: string;
     CheckBoxAll: string;
     CheckBoxOn: string;
     CheckBoxOff: string;
